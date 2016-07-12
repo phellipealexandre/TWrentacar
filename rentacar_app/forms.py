3c60b736b50e89d302dc9c3985de8913c23aee96
Phellipe Silva <phellipealexandre@gmail.com>
2016-07-12 16:10:38 -0300

    Edition of cars and customers implemented

--- rentacar_app/forms.py
+++ rentacar_app/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 
 class CarForm(forms.Form):
+    input_id = None
     input_model = forms.CharField(label='Model', max_length=100)
     input_plate = forms.CharField(label='Plate', max_length=100)
     input_brand = forms.CharField(label='Brand', max_length=100)
@@ -8,6 +9,8 @@ class CarForm(forms.Form):
     input_price = forms.FloatField(label='Price per day')
 
 class CustomerForm(forms.Form):
+    input_id = None
     input_name = forms.CharField(label='Name', max_length=100)
     input_cpf = forms.CharField(label='CPF', max_length=14)
-    input_birthday = forms.DateField(label='Birthday', input_formats=['%d-%m-%Y','%d/%m/%Y', '%d/%m/%y'])
+    input_birthday = forms.DateField(label='Birthday', widget=forms.DateInput(format='%d/%m/%Y'),
+                                     input_formats=['%d-%m-%Y','%d/%m/%Y', '%d/%m/%y'])
--- rentacar_app/views.py
+++ rentacar_app/views.py
@@ -15,6 +15,14 @@ def customers(request):
 def customers_register(request):
     form = CustomerForm()
     context = {'form': form}
+
+    if request.method == 'POST' and request.POST['customer_id']:
+        customer = Customer.objects.get(pk=request.POST['customer_id'])
+        context['customer_id'] = request.POST['customer_id']
+        form.initial = {'input_name': customer.name, 'input_birthday': customer.birthday,
+                        'input_cpf': customer.cpf}
+        form.input_id = customer.pk
+
     return render(request, 'rentacar_app/customers_register.html', context)
 
 def customers_remove(request):
@@ -29,7 +37,12 @@ def customer_submit(request):
         form = CustomerForm(request.POST)
 
         if form.is_valid():
-            customer = Customer()
+            # TODO: Improve how we are passing the customer_id
+            if request.POST['customer_id'] != 'None':
+                customer = Customer.objects.get(pk=request.POST['customer_id'])
+            else:
+                customer = Customer()
+
             customer.name = form.cleaned_data['input_name']
             customer.cpf = form.cleaned_data['input_cpf']
             customer.birthday = form.cleaned_data['input_birthday']
@@ -38,7 +51,7 @@ def customer_submit(request):
             return redirect('customers')
 
         else:
-            return HttpResponse('Error')
+            return HttpResponse('Error. The form is not valid, please try again')
 
 def cars(request):
     cars = Car.objects.all()
@@ -55,6 +68,14 @@ def cars_remove(request):
 def cars_register(request):
     form = CarForm()
     context = {'form': form}
+
+    if request.method == 'POST' and request.POST['car_id']:
+        car = Car.objects.get(pk=request.POST['car_id'])
+        context['car_id'] = request.POST['car_id']
+        form.initial = {'input_model': car.model, 'input_plate': car.plate,
+                        'input_brand': car.brand, 'input_color': car.color, 'input_price': car.price_per_day}
+        form.input_id = car.pk
+
     return render(request, 'rentacar_app/cars_register.html', context)
 
 def car_submit(request):
@@ -62,7 +83,12 @@ def car_submit(request):
         form = CarForm(request.POST)
 
         if form.is_valid():
-            car = Car()
+            # TODO: Improve how we are passing the customer_id
+            if request.POST['car_id'] != 'None':
+                car = Car.objects.get(pk=request.POST['car_id'])
+            else:
+                car = Car()
+
             car.plate = form.cleaned_data['input_plate']
             car.model = form.cleaned_data['input_model']
             car.brand = form.cleaned_data['input_brand']
@@ -72,4 +98,4 @@ def car_submit(request):
 
             return redirect('cars')
         else:
-            return HttpResponse('Error')
+            return HttpResponse('Error. The form is not valid, please try again')
